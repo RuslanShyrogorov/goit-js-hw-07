f1c4438c8e78b5dd4e62dfbee7f1efa0536717a6
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-07-18 01:27:23 +0300

    Add murkup task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+
+function createMarkupGalleryItems(galleryItems) {
+  return galleryItems.map(({ preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+  }).join('')
+}
+
+const galleryMarkup = createMarkupGalleryItems(galleryItems)
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+// const gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
